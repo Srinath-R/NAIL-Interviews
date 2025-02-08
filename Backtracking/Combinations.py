adf59d2abaec1ae3777562882705d74cc85778f1
Srinath R <35762713+Srinath-R@users.noreply.github.com>
2025-02-08 13:29:03 +0530

    Create Combinations.py

---
+++ Backtracking/Combinations.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        res=[]
+        self.dfs(range(1, n+1), k, [], res)
+        return res
+    
+    def dfs(self, nums, k, path, res):
+        if len(path) == k:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], k, path+ [nums[i]], res)
